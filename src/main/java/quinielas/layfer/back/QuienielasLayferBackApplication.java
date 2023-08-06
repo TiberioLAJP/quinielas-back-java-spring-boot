aea0db90fc5aef1ea339df5a59bc5a845b313692
Luis Jimenez <lajp0097@gmail.com>
2023-08-05 22:43:20 -0600

    Create project with dependencies

---
+++ src/main/java/quinielas/layfer/back/QuienielasLayferBackApplication.java
@@ -0,0 +1,13 @@
+package quinielas.layfer.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuienielasLayferBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuienielasLayferBackApplication.class, args);
+	}
+
+}
